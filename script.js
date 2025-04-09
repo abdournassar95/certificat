f0c664cf468268877940e3eac5a14fff3a198caa
عبده نصار <abdou314@live.com>
2025-04-09 16:02:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,11 @@
 
 const sampleData = {
   "502183021": { name: "ALI ABDULKADER",nationality: "SYRIA", rank: "MASTER",photo: "Ali.JPG",certificates: [ { name: "COC", issue: "14-04-2025", expiry: "13-07-2025" } ]},
-  "502183022": {
-    name: "ALI ABDULKADER",
-    nationality: "SYRIA",
-    rank: "MASTER",
-    photo: "Ali.JPG",
-    certificates: [
-      { name: "GMDSS", issue: "14-04-2025", expiry: "13-07-2025" }
-    ]
-  },
-  "511545184": {
-    name: "MOHAMAD BADRAH",
-    nationality: "SYRIA",
-    rank: "CHIEF ENGINEER",
-    photo: "CH ENG.JPG",
-    certificates: [
-      { name: "COC", issue: "14-04-2025", expiry: "13-07-2025" }
-    ]
-  },
-  "55688311": {
-    name: "IBRAHIM HAGGAG ELFEKEY",
-    nationality: "EGYPT",
-    rank: "2ND ENGINEER",
-    photo: "IP.JPG",
-    certificates: [
-      { name: "COC", issue: "04-04-2025", expiry: "03-07-2025" }
-    ]
-  },
-  "574572957": {
-    name: "MAHMOUD ALY",
-    nationality: "EGYPT",
-    rank: "2ND OFFICER",
-    photo: "2ND OFF.JPG",
-    certificates: [
-      { name: "COC", issue: "14-04-2025", expiry: "13-07-2025" }
-    ]
-  },
-  "501303875": {
-    name: "MOHAMAD JAMAL ABDO",
-    nationality: "SYRIA",
-    rank: "CHIEF MATE",
-    photo: "CH OFF.JPG",
-    certificates: [
-      { name: "COC", issue: "14-04-2025", expiry: "13-07-2025" }
-    ]
-  }
-};
+  "502183022": {name: "ALI ABDULKADER",nationality: "SYRIA",rank: "MASTER", photo: "Ali.JPG",certificates: [ { name: "GMDSS", issue: "14-04-2025", expiry: "13-07-2025" } ]},
+  "511545184": {name: "MOHAMAD BADRAH",nationality: "SYRIA",rank: "CHIEF ENGINEER",photo: "CH ENG.JPG",certificates: [{ name: "COC", issue: "14-04-2025", expiry: "13-07-2025" }] },
+  "55688311": {name: "IBRAHIM HAGGAG ELFEKEY",nationality: "EGYPT",rank: "2ND ENGINEER",photo: "IP.JPG",certificates: [{ name: "COC", issue: "04-04-2025", expiry: "03-07-2025" } ] },
+  "574572957": {name: "MAHMOUD ALY",nationality: "EGYPT",rank: "2ND OFFICER",photo: "2ND OFF.JPG",certificates: [{ name: "COC", issue: "14-04-2025", expiry: "13-07-2025" } ]},
+  "501303875": {name: "MOHAMAD JAMAL ABDO",nationality: "SYRIA",rank: "CHIEF MATE",photo: "CH OFF.JPG",certificates: [{ name: "COC", issue: "14-04-2025", expiry: "13-07-2025" }]}};
 
 
 function searchSeafarer() {
