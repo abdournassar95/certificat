a6cc8620635ecf13d266af6d3a3d3567dd1b410f
عبده نصار <abdou314@live.com>
2025-06-01 21:00:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 
 const sampleData = {
   "502183021": { name: "ALI ABDULKADER",nationality: "SYRIA", rank: "MASTER",photo: "Ali.JPG",certificates: [ { name: "COC", issue: "14-04-2025", expiry: "13-07-2025" } ]},
+  "502183021": { name: "ALI ABDULKADER",nationality: "SYRIA", rank: "MASTER",photo: "Ali.JPG",certificates: [ { name: "COC", issue: "14-04-2025", expiry: "13-07-2025" } ]},
+  "5013038501": { name: "AHMED MOHAMED AMIN",nationality: "EGYPT", rank: "CHIEF MATE",photo: "AMIN.JPG",certificates: [ { name: "COC", issue: "22-05-2025", expiry: "21-08-2025" } ]},
+  "5013038503": { name: "AHMED MOHAMED AMIN",nationality: "EGYPT", rank: "CHIEF MATE",photo: "AMIN.JPG",certificates: [ { name: "GMDSS", issue: "22-05-2025", expiry: "21-08-2025" } ]},
+  "55688711": { name: "ELSAYED SHAWKAT ELSAYED",nationality: "EGYPT", rank: "2ND ENGINEER",photo: "SAYED.JPG",certificates: [ { name: "COC", issue:  "22-05-2025", expiry: "21-08-2025" } ]},
   "502183022": {name: "ALI ABDULKADER",nationality: "SYRIA",rank: "MASTER", photo: "Ali.JPG",certificates: [ { name: "GMDSS", issue: "14-04-2025", expiry: "13-07-2025" } ]},
   "511545184": {name: "MOHAMAD BADRAH",nationality: "SYRIA",rank: "CHIEF ENGINEER",photo: "CH ENG.JPG",certificates: [{ name: "COC", issue: "14-04-2025", expiry: "13-07-2025" }] },
   "55688311": {name: "IBRAHIM HAGGAG ELFEKEY",nationality: "EGYPT",rank: "2ND ENGINEER",photo: "IP.JPG",certificates: [{ name: "COC", issue: "04-04-2025", expiry: "03-07-2025" } ] },
